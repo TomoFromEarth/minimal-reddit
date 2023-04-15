17e64c0bf76f955e762e165fdb401cb95d6dd3ad
TomoFromEarth <mikedonothackmethanks@gmail.com>
2023-04-15 00:17:05 -0400

    created getPostComments function

--- minimal-reddit/src/api/reddit.js
+++ minimal-reddit/src/api/reddit.js
@@ -13,3 +13,10 @@ export const getSubreddits = async () => {
 
   return json.data.children.map((subreddit) => subreddit.data);
 };
+
+export const getPostComments = async (permalink) => {
+  const response = await fetch(`${API_ROOT}${permalink}.json`);
+  const json = await response.json();
+
+  return json[1].data.children.map((subreddit) => subreddit.data);
+};
