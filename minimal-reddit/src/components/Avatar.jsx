77dc3ff33dc95c13fe0192e25468be14f9a1d0a6
TomoFromEarth <mikedonothackmethanks@gmail.com>
2023-04-16 07:05:50 -0400

    Created Avatar.jsx component and updated configs and declared propTypes where missing

---
+++ minimal-reddit/src/components/Avatar.jsx
@@ -0,0 +1,13 @@
+import PropTypes from "prop-types";
+
+const Avatar = ({ name }) => {
+  return (
+    <img src={`https://api.adorable.io/avatars/10/${name}`} alt={`${name}'s profile`} className="mr-1 h-5 rounded" />
+  );
+};
+
+Avatar.propTypes = {
+  name: PropTypes.string.isRequired,
+};
+
+export default Avatar;
--- minimal-reddit/src/components/Card.jsx
+++ minimal-reddit/src/components/Card.jsx
@@ -1,5 +1,12 @@
+import PropTypes from "prop-types";
+
 const Card = ({ className, children }) => {
-  return <div className={`bg-slate-50 mb-4 rounded p-3 ${className}`}>{children}</div>;
+  return <div className={`mb-4 rounded bg-slate-50 p-3 ${className}`}>{children}</div>;
+};
+
+Card.propTypes = {
+  className: PropTypes.string.isRequired,
+  children: PropTypes.string.isRequired,
 };
 
 export default Card;
