47b66b76ed19402d104f385137085a0c7bc1b083
TomoFromEarth <mikedonothackmethanks@gmail.com>
2023-04-16 02:16:28 -0400

    up vote button and <p> finished/shortenNumber function created in utils

--- minimal-reddit/src/components/Posts.jsx
+++ minimal-reddit/src/components/Posts.jsx
@@ -1,6 +1,7 @@
 import { useState } from "react";
 import Card from "../components/Card";
 import { TiArrowUpThick, TiArrowUpOutline } from "react-icons/ti";
+import shortenNumber from "../utils/shortenNumber";
 
 const Post = ({ post, onToggleComments }) => {
   const [vote, setVote] = useState(0);
@@ -22,6 +23,17 @@ const Post = ({ post, onToggleComments }) => {
     return <TiArrowUpOutline className="flex items-center rounded p-0 pointer border-none bg-none" />;
   };
 
+  const getVoteType = () => {
+    if (vote === 1) {
+      return "text-green-500";
+    }
+    if (vote === -1) {
+      return "text-red-500";
+    }
+
+    return "";
+  };
+
   return (
     <article key={post.id}>
       <Card>
@@ -29,12 +41,13 @@ const Post = ({ post, onToggleComments }) => {
           <div className="flex flex-col items-center mr-3">
             <button
               type="button"
-              className={`${vote === 1 && "text-green-500"}`}
+              className={`${vote === 1 && "text-green-500 hover:text-green-500 active:text-green-500"}`}
               onClick={() => onHandleVote(1)}
               aria-label="Up vote"
             >
               {renderUpVote()}
             </button>
+            <p className={`text-green-500 ${getVoteType()}`}>{shortenNumber(post.ups, 1)}</p>
           </div>
         </div>
       </Card>
