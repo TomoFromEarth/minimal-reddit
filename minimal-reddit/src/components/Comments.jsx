b0c2d57e6194ea24c5e9d14bf53cf876603c8723
TomoFromEarth <82728892+TomoFromEarth@users.noreply.github.com>
2023-04-16 07:30:11 -0400

    created Comments component (#11)

---
+++ minimal-reddit/src/components/Comments.jsx
@@ -0,0 +1,25 @@
+import { formatDistanceToNow } from "date-fns";
+import PropTypes from "prop-types";
+import ReactMarkdown from "react-markdown";
+import Avatar from "../components/Avatar";
+
+const Comments = ({ comment }) => {
+  return (
+    <div className="m-0 rounded p-2">
+      <div className="mb-2 flex items-center">
+        <Avatar name={comment.author} />
+        <p className="font-bold">{comment.author}</p>
+        <p className="ml-auto italic">
+          {formatDistanceToNow(new Date(comment.created_utc * 1000), { addSuffix: true })}
+        </p>
+      </div>
+      <ReactMarkdown source={comment.body} />
+    </div>
+  );
+};
+
+Comments.propTypes = {
+  comment: PropTypes.string.isRequired,
+};
+
+export default Comments;
