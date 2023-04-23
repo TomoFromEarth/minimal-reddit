3f51cdf0fdbe039df6bcc6d23e733cb2394fbdb7
TomoFromEarth <82728892+TomoFromEarth@users.noreply.github.com>
2023-04-23 13:21:37 -0400

    fixed search feature/comments now render (#18)

--- minimal-reddit/src/components/Comments.jsx
+++ minimal-reddit/src/components/Comments.jsx
@@ -4,22 +4,29 @@ import ReactMarkdown from "react-markdown";
 import Avatar from "./Avatar";
 
 const Comments = ({ comment }) => {
+  const date = new Date(comment.created_utc * 1000);
+  const isValidDate = !isNaN(date.getTime());
+  const formattedDate = isValidDate ? formatDistanceToNow(date, { addSuffix: true }) : "Invalid Date";
+
+  //Stops comments with invalid dates from API from rendering
+  if (!isValidDate) {
+    return null;
+  }
+
   return (
     <div className="m-0 rounded-lg p-2">
       <div className="mb-2 flex items-center">
         <Avatar name={comment.author} />
         <p className="font-bold">{comment.author}</p>
-        <p className="ml-auto italic">
-          {formatDistanceToNow(new Date(comment.created_utc * 1000), { addSuffix: true })}
-        </p>
+        <p className="ml-auto italic">{formattedDate}</p>
       </div>
-      <ReactMarkdown source={comment.body} />
+      <ReactMarkdown>{comment.body}</ReactMarkdown>
     </div>
   );
 };
 
 Comments.propTypes = {
-  comment: PropTypes.string.isRequired,
+  comment: PropTypes.object.isRequired,
 };
 
 export default Comments;
