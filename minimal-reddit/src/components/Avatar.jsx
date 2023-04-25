cf9e12d88aa156d9a299d57e78e4cda369a26f1b
TomoFromEarth <mikedonothackmethanks@gmail.com>
2023-04-25 08:50:34 -0400

    posts/card/comments mobile layout done

--- minimal-reddit/src/components/Avatar.jsx
+++ minimal-reddit/src/components/Avatar.jsx
@@ -4,7 +4,7 @@ const Avatar = ({ name }) => {
   const seed = Math.random().toString(36).substring(7);
   const avatar = `https://api.dicebear.com/6.x/pixel-art/svg?seed=${seed}`;
 
-  return <img src={`${avatar}`} alt={`${name}'s profile`} className="mr-1 h-8 w-8 rounded-lg" />;
+  return <img src={`${avatar}`} alt={`${name}'s profile`} className="h-6  w-6 rounded-lg md:mr-1 md:h-8 md:w-8" />;
 };
 
 Avatar.propTypes = {
